c8b83f5e946ec1b5b130a41f0eecd57d4e817762
muskan245verma <69051719+muskan245verma@users.noreply.github.com>
2021-09-01 17:06:08 +0530

    Update flask_test.py

--- flask_test.py
+++ flask_test.py
@@ -4,9 +4,6 @@ import Lgame
 app = Flask(__name__, template_folder='templates')
 
 
-@app.route('/')
-def home():
-    return render_template('home.html')
 @app.route('/index')
 def index():
     return render_template('index.html')
